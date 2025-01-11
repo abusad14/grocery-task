71ccbab575afe18df07f8a218c127f459f9b2782
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-11 22:15:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskGrocery from "./TaskGrocery";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <TaskGrocery />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/TaskGrocery.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import allGroceryData from "./grocery.json";
+const TaskGrocery = () => {
+  return (
+    <div>
+      <div className="flex justify-between bg-zinc-400 p-5">
+        <h1 className="bg-green-300 p-2 text-xl">
+          This color shows price between 10 to 15
+        </h1>
+        <h1 className="bg-blue-300 p-2 text-xl">
+          This color shows price between 15 to 20
+        </h1>
+        <h1 className="bg-red-300 p-2 text-xl">
+          This color shows price more than 20
+        </h1>
+      </div>
+      <div className=" flex flex-wrap w-full justify-center">
+        {allGroceryData.map((v, i) => {
+          let bgColor = "";
+          if (v.price > 10 && v.price <= 15) {
+            bgColor = "bg-green-300";
+          } else if (v.price > 15 && v.price <= 20) {
+            bgColor = "bg-blue-300";
+          } else {
+            bgColor = "bg-red-300";
+          }
+          return (
+            <div
+              key={v.title}
+              className={`${bgColor} p-5 border m-5 w-80 shadow-lg hover:scale-105 hover:shadow-2xl hover:cursor-pointer`}
+            >
+              <h1 className="text-3xl">{v.title}</h1>
+              <h1>{v.description}</h1>
+              <h1 className="text-2xl font-semibold">Price : {v.price}$</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TaskGrocery;
